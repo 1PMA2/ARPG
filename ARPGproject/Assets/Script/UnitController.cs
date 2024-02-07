85428155cbd146ca10a2806f96246f6489869b64
1PMA2 <sokad000@gmail.com>
2024-02-07 21:02:51 +0900

    character controller 수정

--- ARPGproject/Assets/Script/UnitController.cs
+++ ARPGproject/Assets/Script/UnitController.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class UnitController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
-    private Transform unitBody;
-    [SerializeField]
-    private Transform unitCamera;
+    private GameObject cameraPrefeb;
     [SerializeField]
     private Transform cameraArm;
+    [SerializeField]
+    private Transform unitCamera;
 
     [SerializeField]
     private Quaternion targetRotation;
@@ -20,34 +21,64 @@ public class UnitController : MonoBehaviour
     private Vector2 keyDelta = Vector2.zero;
 
     [SerializeField]
-    private float rotationSpeed = 150;
+    private float rotationSpeed = 150f;
     [SerializeField]
     private Vector2 turnAngle = new Vector2(10, 30);
     [SerializeField] 
-    private float Distance = -4;
+    private float Distance = -4f;
 
-    Animator animator;
-    Rigidbody unitRigidbody;
+    [SerializeField]
+    private float jumpPower = 10f;
+
+    private Animator animator;
+    private CharacterController characterController;
+
+    [SerializeField]
+    Vector3 inputDir;
 
     //Animator animator;
     void Start()
     {
-        animator = unitBody.GetComponent<Animator>();
-        unitRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+
+        cameraArm = Instantiate(cameraPrefeb).transform;
+        unitCamera = cameraArm.GetChild(0);
+
+        Application.targetFrameRate = 144;
+    }
+
+    private void FixedUpdate()
+    {
+       
+        //unitRigidbody.MovePosition(transform.position + inputDir * Time.deltaTime * 5f);
+        //unitRigidbody.AddForce(inputDir * 100f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        LookAround();
-        Move();
+        MoveDiraction();
+
+        
+
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Time.timeScale = 0.1f;
+        }
+        else
+            Time.timeScale = 1;
     }
-    private void Move()
+
+    private void LateUpdate()
     {
+        LookAround();
+    }
 
-
-
-        bool isMove = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || 
+    private void MoveDiraction()
+    {
+        bool isMove = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) ||
             Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow);
 
         float hAxis = 0f;
@@ -61,28 +92,25 @@ public class UnitController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftArrow)) { hAxis = -1f; }
 
-
         animator.SetBool("isMove", isMove);
 
+        Vector3 cameraForward = unitCamera.forward;
+        Vector3 cameraRight = unitCamera.right;
 
-        Vector3 cameraForward = unitCamera.forward; // ?????? ???? ????
-        Vector3 cameraRight = unitCamera.right;     // ?????? ?????? ????
+        inputDir = cameraForward * vAxis + cameraRight * hAxis;
 
-        Vector3 inputDir = cameraForward * vAxis + cameraRight * hAxis;
-
-        inputDir.y = 0f;
+        inputDir.y = 0;
         inputDir = inputDir.normalized;
 
-        // ĳ????? ?????? ?????? ??????? ????????.
         if (inputDir != Vector3.zero)
         {
             Quaternion moveRotation = Quaternion.LookRotation(inputDir);
 
-            // ?ε??? ?????? ????????.
-            unitBody.rotation = Quaternion.Slerp(unitBody.rotation, moveRotation, 20f * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, moveRotation, 20f * Time.deltaTime);
         }
 
-        unitRigidbody.velocity = inputDir * 5;
+
+        characterController.Move(inputDir * Time.deltaTime * 5);
     }
 
     private void LookAround()
@@ -116,14 +144,22 @@ public class UnitController : MonoBehaviour
             keyDelta.y += turnAngle.y;
         }
 
+        keyDelta.x %= 360f;
+        keyDelta.y %= 360f;
+
         targetRotation = Quaternion.Euler(keyDelta.y, keyDelta.x, 0);
         cameraArm.rotation = Quaternion.Euler(camAngle.x, camAngle.y, 0); //z축 회전 고정
-
         // 시간을 사용하여 회전 보간
-        cameraArm.rotation = Quaternion.Slerp(cameraArm.rotation, targetRotation, Time.deltaTime * 5f);
+        cameraArm.rotation = Quaternion.Slerp(cameraArm.rotation, targetRotation, Time.deltaTime * 10f);
 
+        // 스무딩 카메라
+        //Vector3 targetPos = new Vector3(transform.position.x, transform.position.y + cameraArm.position.y, transform.position.z);
+        cameraArm.position = Vector3.Lerp(cameraArm.position, transform.position, Time.deltaTime * 5f);
+        cameraArm.position = new Vector3(cameraArm.position.x, transform.position.y, cameraArm.position.z);
+
+        //휠로 캐릭터 줌
         Distance += Input.GetAxis("Mouse ScrollWheel") * 5;
-        unitCamera.localPosition = new Vector3(0, 0, Distance);
+        unitCamera.localPosition = new Vector3(0, 2, Distance);
 
     }
 
